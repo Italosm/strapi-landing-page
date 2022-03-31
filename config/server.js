39602cba7877bc6c266118dfaac84b925f6063b0
Italo Souza Melo <italosm29@gmail.com>
2022-03-31 20:21:18 -0300

    att .env

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'cac22fe1bb19e76e033e40bda251353f'),
+      secret: env("ADMIN_JWT_SECRET"),
     },
   },
 });
